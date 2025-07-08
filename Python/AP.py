868aa8385d497035681032df6b2be64fb35ce135
Adi-God-7 <manorama.rajkumar@gmail.com>
2025-07-08 16:45:00 +0530

    Delete Python directory

--- Python/AP.py
+++
@@ -1,6 +0,0 @@
-a=int(input("Enter the first term="))
-d=int(input("Enter the common difference="))
-n=int(input("Enter the no. of terms="))
-print("AP=",end="")
-for i in range(0,n):
-    print(a+i*d,",",end="")
--- Python/bubblesort.py
+++
@@ -1,7 +0,0 @@
-List=eval(input("Enter the list:"))
-length=len(List)
-for i in range(length):
-    for j in range(length -1 -i):
-        if List[j]>List[j+1]:
-            List[j],List[j+1]=List[j+1],List[j]
-print(List)
